2c6df1b72d9a26a74a87e769eaf4e560c07c3ece
blazeknight36 <blazeknight36@gmail.com>
2026-01-21 21:03:46 -0500

    Added wine and rolled forward packages

--- TheGrantCube/configuration.nix
+++ TheGrantCube/configuration.nix
@@ -112,6 +112,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    wineWowPackages.stable
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
